6bda61f0ff12c328a54ae0876681685db5d70421
Tien Huu Nguyen <tienguyen@strongtie.com>
2023-11-22 14:06:40 +0700

    fix: detect faces

--- tienguyen/main3.py
+++ tienguyen/main3.py
@@ -98,6 +98,7 @@ root = tk.Tk()
 def add_user():
     name = name_entry.get()
     cap = cv2.VideoCapture(0)
+    cv2.namedWindow('Capture User Face', cv2.WINDOW_NORMAL)
     while cap.isOpened():
         success, image = cap.read()
         if not success:
@@ -123,6 +124,7 @@ def add_user():
                 cv2.putText(image, name, (x, y + h + 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
         # Display the image
+        cv2.resizeWindow('Capture User Face', 640, 360)
         cv2.imshow('Capture User Face', image)
         if cv2.waitKey(5) & 0xFF == 27:
             break
@@ -137,6 +139,8 @@ def add_user():
             cap.release()
             cv2.destroyWindow('Capture User Face')
 
+# ... (rest of the code)
+
 def identify_face(image):
     # Convert the image from BGR to RGB
     # rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -148,27 +152,33 @@ def identify_face(image):
     # rgb_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2BGR)
 
     for face_location in face_locations:
+        print(face_location)
         top, right, bottom, left = face_location
         face_image = image[top:bottom, left:right]
-
+        print(face_image)
         # Loop over all known faces
         for filename in os.listdir('known_faces'):
             if filename.endswith('.jpg'):
                 user_name = os.path.splitext(filename)[0]
+                print(user_name)
                 user_image = cv2.imread(os.path.join('known_faces', filename))
-                print(user_name, user_image)
+
                 # Convert the user image from BGR to RGB
                 rgb_user_image = cv2.cvtColor(user_image, cv2.COLOR_BGR2RGB)
 
                 # Create a face encoding for the user image
                 user_face_encoding = face_recognition.face_encodings(rgb_user_image)[0]
 
-                # Create a face encoding for the face image
-                face_encoding = face_recognition.face_encodings(face_image)[0]
+                # Find the face landmarks
+                # face_landmarks = face_recognition.face_landmarks(face_image)
+
+                # If face landmarks are found, compute the face encoding
+                # if face_landmarks:
+                face_encoding = face_recognition.face_encodings(image, known_face_locations=[face_location])[0]
 
                 # Compare the face encoding with the user face encoding
                 match = face_recognition.compare_faces([user_face_encoding], face_encoding)
-
+                print(match)
                 if match[0]:
                     return user_name
 
@@ -176,10 +186,14 @@ def identify_face(image):
 
 def detect_faces():
     cap = cv2.VideoCapture(0)
+    cv2.namedWindow('MediaPipe Face Detection', cv2.WINDOW_NORMAL)
+
     while cap.isOpened():
         success, image = cap.read()
         if not success:
             break
+
+        image = cv2.resize(image, (640, 360))
 
         # Convert the BGR image to RGB
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -194,12 +208,15 @@ def detect_faces():
                 # For now, we'll just use a placeholder function
                 user_name = identify_face(image)
 
+                # Convert the image back to BGR color space
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
                 # Draw a box around the face
                 x = int(detection.location_data.relative_bounding_box.xmin * image.shape[1])
                 y = int(detection.location_data.relative_bounding_box.ymin * image.shape[0])
                 w = int(detection.location_data.relative_bounding_box.width * image.shape[1])
                 h = int(detection.location_data.relative_bounding_box.height * image.shape[0])
-                if user_name in user_data:
+                if user_name:
                     color = (0, 255, 0)  # Green
                 else:
                     color = (0, 0, 255)  # Red
@@ -209,6 +226,7 @@ def detect_faces():
                 cv2.putText(image, user_name, (x, y + h + 20), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
 
         # Display the image
+        cv2.resizeWindow('MediaPipe Face Detection', 640, 360)
         cv2.imshow('MediaPipe Face Detection', image)
         if cv2.waitKey(5) & 0xFF == 27:
             break
